25d78e9bacc67cbf44ba7e6b79779c3764fc0e35
0xJs <43987245+0xJs@users.noreply.github.com>
2022-02-15 13:04:22 +0100

    Create fuzzing.py

---
+++ infrastructure/fuzzing.py
@@ -0,0 +1,27 @@
+import socket, time, sys
+
+ip = "<IP>"
+port = <PORT>
+prefix = ""
+timeout = 5
+
+buffer = []
+counter = 100
+while len(buffer) < 30:
+    buffer.append("A" * counter)
+    counter += 100
+
+for string in buffer:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        connect = s.connect((ip, port))
+        s.recv(1024)
+        print("Fuzzing with %s bytes" % len(string))
+        s.send(prefix + string + "\r\n")
+        s.recv(1024)
+        s.close()
+    except:
+        print("Could not connect to " + ip + ":" + str(port))
+        sys.exit(0)
+    time.sleep(1)
